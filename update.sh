6f1f63f5ffed94f328c9a2b1ac3abaf67176543b
sakurum <sakuranomiyu@gmail.com>
2020-09-11 11:19:09 +0900

    update sh

--- update.sh
+++ update.sh
@@ -3,9 +3,9 @@
 git config --global user.name "sakurum"
 git config --global user.email "sakuranomiyu@gmail.com"
 
-git remote set-url origin https://github.com/sakurum/covid19-mie-getdata.git
+git remote set-url origin https://sakurum:${UPDATE_TOKEN}github.com/sakurum/covid19-mie-getdata.git
 
 git checkout master
-
-date = $(date +%Y/%m/%d)
-git commit -m "[updater] update data (${date})"
+git stage data/*
+git commit -m "[updater] `date +%Y/%m/%d` の更新"
+git push
