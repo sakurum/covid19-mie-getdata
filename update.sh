88da95d2c2f10757d8efbd7ce1f5a379766ecf20
Sou Okayama <34621864+sakurum@users.noreply.github.com>
2020-09-11 13:03:31 +0900

    fix typo

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 git config --global user.name "sakurum"
 git config --global user.email "sakuranomiyu@gmail.com"
 
-git remote set-url origin https://sakurum:${UPDATE_TOKEN}github.com/sakurum/covid19-mie-getdata.git
+git remote set-url origin https://sakurum:${UPDATE_TOKEN}@github.com/sakurum/covid19-mie-getdata.git
 
 git checkout master
 git stage data/*
